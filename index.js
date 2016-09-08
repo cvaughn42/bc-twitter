cb74cdbe6cabc23c960da1bc7ebe39c128804a6d
cvaughn42 <cvaughn42@comcast.net>
2016-09-08 06:57:56 -0400

    Initial Commit with basic files in place

---
+++ index.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+var port = 8080;
+
+function readFile(fileName, encoding) {
+
+    return new Promise(function(resolve, reject) {
+
+        fs.readFile(fileName, encoding, function(err, contents) {
+            if (err)
+            {
+                reject(err);
+            }
+            else
+            {
+                resolve(contents);
+            }
+        });
+    });
+};
+
+app.get('/', function (req, res) {
+
+    var out = "";
+
+    readFile('header.html', 'utf8').then(
+        function(html){
+            out += html;
+            out += "Welcome!";
+            return readFile('footer.html', 'utf8');
+        }
+    ).then(
+        function(html) {
+            out += html;
+            res.send(out);
+        }
+    ).catch(
+        function(err) {
+            console.log("Unable to send html: " + err);
+        }
+    );
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
